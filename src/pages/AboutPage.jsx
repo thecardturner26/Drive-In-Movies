0e95d5afb2a09fc7111c11d65e2c049988f16dc1
Marco Arteaga <marcoarteaga26@gmail.com>
2020-01-27 23:24:54 -0600

    chnages to about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -17,15 +17,19 @@ function AboutPage(props) {
             {/* <Grid> */}
                 <Row style={{alignContent: "center"}}>
                     <Col xs={6} md={3}>
+                        
                         <Image src="https://i.imgur.com/zMeKFos.jpg" style={{width: '11em', height: "18em", borderRadius: '10%'}}/>
                     </Col>
                     <Col xs={6} md={3}>
+                        
                         <Image src="https://i.imgur.com/2Pw7YNC.jpg" style={{width: '11em', height: "18em", borderRadius: '10%'}}/>
                     </Col>
                     <Col xs={6} md={3}>
+                       
                         <Image src="https://i.imgur.com/RufgRwl.jpg" style={{width: '11em', height: "18em", borderRadius: '10%'}}/>
                     </Col>
                     <Col xs={6} md={3}>
+                       
                         <Image src="https://i.imgur.com/XHNV73z.jpg" style={{width: '11em', height: "18em", borderRadius: '10%'}}/>
                     </Col>
                 </Row>
--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -41,7 +41,7 @@ class ContactPage extends React.Component {
             disabled: true
         });
 
-        Axios.post('http://localhost:3030/api/email', this.state)
+        Axios.post('http://localhost:3000', this.state)
             .then(res => {
                 if(res.data.success) {
                     this.setState({
